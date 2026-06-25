276a1caf42ead5efbf03078d1a3974a64c9c03f3
Tuomas Katila <tuomas.katila@intel.com>
2026-06-25 13:11:59 +0300

    release 0.3.0-rc1
    Signed-off-by: Tuomas Katila <tuomas.katila@intel.com>

--- test/e2e/e2e_suite_test.go
+++ test/e2e/e2e_suite_test.go
@@ -38,7 +38,7 @@ var (
 
 	// projectImage is the name of the image which will be build and loaded
 	// with the code source changes to be tested.
-	projectImage = "ghcr.io/intel/intel-gpu-base-operator:0.3.0-rc0"
+	projectImage = "ghcr.io/intel/intel-gpu-base-operator:0.3.0-rc1"
 )
 
 // TestE2E runs the end-to-end (e2e) test suite for the project. These tests execute in an isolated,
